364241db6163129731ccb16477a36ada31a76e83
ioqy <95605581+ioqy@users.noreply.github.com>
2022-11-26 22:36:06 +0100

    Initial commit

---
+++ example-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# For root put unit files into /etc/systemd/system
+
+# Service as root
+sudo systemctl daemon-reload
+sudo systemctl start example.service
+sudo systemctl enable example.service
+systemctl status example.service
+
+# Timer as root
+sudo systemctl daemon-reload
+sudo systemctl enable --now example.timer
+systemctl status automated-backup.timer
+
+# List timers for root
+systemctl list-timers --all
+
+# For user put unit files into /home/user/.config/systemd/user
+
+# Service as user
+systemctl --user daemon-reload
+systemctl --user start example.service
+systemctl --user enable example.service
+systemctl status --user example.service
+
+# Timer as user
+systemctl --user daemon-reload
+systemctl --user enable --now example.timer
+systemctl --user status automated-backup.timer
+
+# List timers for user
+systemctl --user list-timers --all
